1c672695062377b937dd0abc9bdd73775a0befc5
saurabh-magar <magarsaurabh59@gmail.com>
2025-01-07 11:59:36 +0530

    added a count button

---
+++ script.js
@@ -0,0 +1,9 @@
+var button=document.getElementById("count");
+let count=0;
+button.addEventListener("click",()=>{
+    count++;
+    let countElement=document.getElementById("count");
+    countElement.innerText=count
+
+
+})
